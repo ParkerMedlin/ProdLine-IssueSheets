79e7d0d7a6d562b3c1db4af970326711fe60f03e
ParkerMedlin <pmedlinkinpakinc@gmail.com>
2021-12-07 07:37:10 -0600

    first commit

---
+++ copyProdSchedule.bat
@@ -0,0 +1,13 @@
+cd C:\OD\Kinpak, Inc\Production - Documents\Production Schedule
+
+copy "Starbrite KPK production schedule.xlsb" "C:\OD\Kinpak, Inc\Blending - Documents\03 Projects\ProdLine-IssueSheets\"
+
+cd C:\OD\Kinpak, Inc\Blending - Documents\03 Projects\ProdLine-IssueSheets\
+
+del ProdScheduleCopy.xlsb
+
+rename "C:\OD\Kinpak, Inc\Blending - Documents\03 Projects\ProdLine-IssueSheets\Starbrite KPK production schedule.xlsb" "ProdScheduleCopy.xlsb"
+
+EXCEL.EXE /e "ProdScheduleCopy.xlsb" 
+
+EXCEL.EXE /e "ISSUESHEET_GEN.xlsb"
